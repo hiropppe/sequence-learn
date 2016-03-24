7017d655cb1c8c7e7e9c3bdc33fcc43f88d14586
hiropppe <hiroki.takeshita@jp21.com>
2016-03-24 17:39:33 +0900

    Rename ner feature script

--- example/event/train.sh
+++ example/event/train.sh
@@ -4,7 +4,7 @@ echo "[CRFSuite Cross Validation]"
 echo "Convert to brat to CoNLL format"
 ./conv_conll.sh ./data/ train.conll.txt [0-9]{4}
 echo "Generate features"
-cat ./data/train.conll.txt | python ./feature/chunking.py > ./data/train.crfsuite.txt
+cat ./data/train.conll.txt | python ./feature/ner.py > ./data/train.crfsuite.txt
 echo "Train CRF model"
 crfsuite learn -m ./data/model -l -L ./data/train.log ./data/train.crfsuite.txt
 #crfsuite learn -g5 -x -l -L ./data/train.log ./data/train.crfsuite.txt
